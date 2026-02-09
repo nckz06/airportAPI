c171d37223999d2c6f39ee3b60ba323340049382
Nicolas Nogueira <nsn.nicolas@gmail.com>
2026-02-09 13:30:22 -0300

    Primeiro commit do projeto Airport API

---
+++ airports/src/main/java/br/gm/nicolas/airports/service/AirportService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gm.nicolas.airports.service;
+
+/**
+ *
+ * @author digma
+ */
+public class AirportService {
+    
+}
---
+++ airports/src/test/java/br/gm/nicolas/airports/AirportsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gm.nicolas.airports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
